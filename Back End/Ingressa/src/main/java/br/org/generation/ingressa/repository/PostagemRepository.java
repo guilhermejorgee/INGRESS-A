eb6c6b3253efea909dc23147800971cf40ab7d06
Guilherme <guilherme.silva307@fatec.sp.gov.br>
2021-06-10 15:53:02 -0300

    ranking empregadores

--- Back End/Ingressa/src/main/java/br/org/generation/ingressa/repository/PostagemRepository.java
+++ Back End/Ingressa/src/main/java/br/org/generation/ingressa/repository/PostagemRepository.java
@@ -3,6 +3,8 @@ package br.org.generation.ingressa.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import br.org.generation.ingressa.model.Postagem;
@@ -12,4 +14,9 @@ public interface PostagemRepository extends JpaRepository <Postagem, Long> {
 	public List<Postagem> findAllByTituloContainingIgnoreCase(String titulo);
 	public List<Postagem> findAllByCargoContainingIgnoreCase(String cargo);
 	public List<Postagem> findAllByRegiaoContainingIgnoreCase(String regiao);
+	
+	// 
+	
+	@Query(value = "select count(usuario_id) from tb_postagem where cargo is not null && usuario_id = :id", nativeQuery = true)
+	public int countPosts(@Param("id") long id);
 }
--- Back End/Ingressa/src/main/java/br/org/generation/ingressa/repository/UsuarioRepository.java
+++ Back End/Ingressa/src/main/java/br/org/generation/ingressa/repository/UsuarioRepository.java
@@ -1,5 +1,6 @@
 package br.org.generation.ingressa.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,7 +8,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.org.generation.ingressa.model.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario,Long>{
-	public Optional<Usuario> findByEmail(String email);
+	
+	public Optional<Usuario> findByEmail(String email);	
+
+	public List<Usuario> findByUsuarioEmpregador(boolean empregador);
+
 	
 
 }
